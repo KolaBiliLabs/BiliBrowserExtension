0015be550971e4c32cc9dfb4094665f5fbc61dd6
Du Cheng <easyhoo2426@163.com>
2024-02-10 13:16:57 +0800

    feat: build

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import path from 'node:path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import electron from 'vite-plugin-electron/simple'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src/*')
+    }
+  },
+  plugins: [
+    react(),
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`.
+        entry: 'electron/main.ts'
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`.
+        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
+        input: path.join(__dirname, 'electron/preload.ts')
+      },
+      // Ployfill the Electron and Node.js built-in modules for Renderer process.
+      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
+      renderer: {}
+    })
+  ]
+})
