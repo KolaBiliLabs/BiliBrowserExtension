c49a80f22bf4f2ef5c782cc3392219e0e665fa47
imddc <easyhoo2426@163.com>
2025-07-29 21:39:08 +0800

    feat: 增加未连接时 ui

---
+++ components/UnlinkView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { sendCheckConnection } from '@/utils';
+import { NButton, NSpace } from 'naive-ui';
+
+
+async function reConnected() {
+  await sendCheckConnection((connected: boolean) => {
+    console.log(connected)
+  })
+}
+</script>
+
+<template>
+  <NSpace vertical :size="15" align="center">
+    当前链接已断开
+    <NButton block type="warning" @click="reConnected">尝试重新连接</NButton>
+  </NSpace>
+</template>
